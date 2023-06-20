f395b11f115973373724a2e2109599809b7cb287
Finn <contact@bechinka.eu>
2023-06-21 00:51:36 +0200

    feat: create initial vetripharm spider

---
+++ PIMS/PIMS/spiders/Vetripharm.py
@@ -0,0 +1,46 @@
+from scrapy.loader import ItemLoader
+from PIMS.spiders.base import BaseSpider
+from scrapy import Spider, Request
+from PIMS.items import Product
+from time import sleep
+import json
+
+
+class VetripharmSpider(BaseSpider):
+    custom_settings = {
+        # "DOWNLOAD_DELAY": "1.5",
+        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0"
+    }
+
+    name = 'Vetripharm'
+    address = '7017900'
+    allowed_domains = ['vetripharm.de']
+    start_urls = ['https://vetripharm.de/de/']
+
+    visited_products = []
+    visited_variations = []
+
+    def search_json_item(self, contained_str, page, script_selector):
+        json_item = None
+        for script in page.css(script_selector).getall():
+            if contained_str in script:
+                json_item = json.loads(script)
+        return json_item
+
+    def parse(self, response):
+        pass
+
+    def parse_pages(self, response):
+        pass
+
+
+    def parse_category(self, response):
+        pass
+
+
+    def parse_variation(self, response):
+        pass
+
+
+    def parse_product(self, response, parent, variation):
+        pass
