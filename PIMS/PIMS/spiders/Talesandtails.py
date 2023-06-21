20588b1047d1530905c6e17d186400389fc17b89
Finn <contact@bechinka.eu>
2023-06-21 13:06:02 +0200

    fix: increase download delay

--- PIMS/PIMS/spiders/Talesandtails.py
+++ PIMS/PIMS/spiders/Talesandtails.py
@@ -8,7 +8,7 @@ import json
 
 class TalesandtailsSpider(BaseSpider):
     custom_settings = {
-        "DOWNLOAD_DELAY": "1.5",
+        "DOWNLOAD_DELAY": "2.5",
         "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0"
     }
 
